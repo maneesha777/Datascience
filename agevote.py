2a5e58adbeed283071dc78e2c727c77eee3f1ba2
maneesha777 <118017166+maneesha777@users.noreply.github.com>
2022-11-15 19:13:04 +0530

    Update agevote.py

--- agevote.py
+++ agevote.py
@@ -1,3 +1,5 @@
+# Write a code to take the age of person as an input and if age >= 18 display "I can vote". If age is < 18 display "I can't vote".
+
 age = int(input("Enter your age: "))   # Input your age
 if age >= 18:                          # Check whether your age is greater than or equal to 18
     print( "I can vote.")
