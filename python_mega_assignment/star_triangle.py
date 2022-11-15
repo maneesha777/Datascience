5aabdc2c5eb24423046d20d897ec8ffeb9a45cac
maneesha777 <118017166+maneesha777@users.noreply.github.com>
2022-11-15 23:12:32 +0530

    Update star_triangle.py

--- python_mega_assignment/star_triangle.py
+++ python_mega_assignment/star_triangle.py
@@ -1,1 +1,15 @@
+''' Write a python program to print below pattern.
 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+'''
+n = int(input("Enter no. of rows: "))        
+for row in range(n):
+    for i in range(n-row-1):
+        print( end=' ')
+    for i in range(row+1):
+        print("*", end=' ')
+    print()
