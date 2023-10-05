9ec92ad3f9882dfb896d157034408bd5dc31c4f0
maneesha777 <118017166+maneesha777@users.noreply.github.com>
2023-10-06 05:13:23 +0530

    Update 80_armstrong_number.py

--- python_mega_assignment/80_armstrong_number.py
+++ python_mega_assignment/80_armstrong_number.py
@@ -1,13 +1,15 @@
 # Write a Python program to check Armstrong Number.
 
 num = int(input("Enter any number: "))   # Input any number
-sum = 0        
+r = len(str(num))
+sum = 0
 temp = num
 while temp>0:
-    n1 = temp % 10
-    sum += n1**3
-    temp //= 10
+      n1 = temp % 10
+      sum += n1**r
+      temp //= 10
 if sum == num:
-    print(num,"is a armstrong number.") 
+    print(num,"is a armstrong number.")
 else:
-    print(num,"is not a armstrong number.") 
+    print(num, "is not armstrong number")
+
